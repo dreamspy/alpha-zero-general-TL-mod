489dbe145b14682ad9223428bb81c4d9513beed3
Unknown <laukur@gmail.com>
2018-09-10 16:23:47 +0000

    Othello human player more safe

--- othello/OthelloPlayers.py
+++ othello/OthelloPlayers.py
@@ -25,14 +25,15 @@ class HumanOthelloPlayer():
                 print(int(i/self.game.n), int(i%self.game.n))
         while True:
             a = input()
-
-            x,y = [int(x) for x in a.split(' ')]
-            a = self.game.n * x + y if x!= -1 else self.game.n ** 2
-            if valid[a]:
-                break
-            else:
+            try:
+                x,y = [int(x) for x in a.split(' ')]
+                a = self.game.n * x + y if x!= -1 else self.game.n ** 2
+                if valid[a]:
+                    break
+                else:
+                    print('Invalid')
+            except ValueError:
                 print('Invalid')
-
         return a
 
 
