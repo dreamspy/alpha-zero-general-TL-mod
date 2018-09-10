bbe02068916a824ffe91f010bcaa29aa09d562e9
Unknown <laukur@gmail.com>
2018-09-10 16:24:03 +0000

    TicTacToe Greedy player added

--- tictactoe/TicTacToePlayers.py
+++ tictactoe/TicTacToePlayers.py
@@ -45,3 +45,19 @@ class HumanTicTacToePlayer():
                 print('Invalid')
 
         return a
+
+class GreedyTicTacToePlayer():
+    def __init__(self, game):
+        self.game = game
+
+    def play(self, board):
+        valids = self.game.getValidMoves(board, 1)
+        candidates = []
+        for a in range(self.game.getActionSize()):
+            if valids[a]==0:
+                continue
+            nextBoard, _ = self.game.getNextState(board, 1, a)
+            score = self.game.getScore(nextBoard, 1)
+            candidates += [(-score, a)]
+        candidates.sort()
+        return candidates[0][1]
