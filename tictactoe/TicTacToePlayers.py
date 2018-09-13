6cd7dfd5ff023c9b3473bcd63dd398319a417c74
Unknown <laukur@gmail.com>
2018-09-13 19:44:33 +0000

    TicTacToe - assure input is valid for human player

--- tictactoe/TicTacToePlayers.py
+++ tictactoe/TicTacToePlayers.py
@@ -31,19 +31,23 @@ class HumanPlayer():
         for i in range(len(valid)):
             if valid[i]:
                 print(int(i/self.game.n), int(i%self.game.n))
-        while True: 
+        while True:
+            # x = ""
+            # y = ""
+
+
             # Python 3.x
             a = input()
             # Python 2.x 
             # a = raw_input()
 
             x,y = [int(x) for x in a.split(' ')]
-            a = self.game.n * x + y if x!= -1 else self.game.n ** 2
-            if valid[a]:
-                break
-            else:
-                print('Invalid')
 
+            if isinstance(x, int) or isinstance(y, int):
+                a = self.game.n * x + y if x!= -1 else self.game.n ** 2
+                if a <= len(valid) - 2 and valid[a]:
+                    break
+            print('Invalid')
         return a
 
 class GreedyPlayer():
